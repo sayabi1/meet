2b236f80fc8c8cd46a9155989dc083d2518a93fb
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-10-16 14:50:03 +0200

    updated function with the numberofEvents

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from "react";
+
 import "./App.css";
 import EventList from "./EventList";
+import NumberOfEvents from "./NumberOfEvents";
 import CitySearch from "./CitySearch";
+import { getEvents, extractLocations } from "./api";
+import "./nprogress.css";
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    seletedLocation: "all",
+    numOfEvents: 16,
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  updateEvents = (location, eventCount) => {
+    if (location === undefined) {
+      location = this.state.seletedLocation;
+    }
+    if (eventCount === undefined) {
+      eventCount = this.state.numOfEvents;
+    }
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+
+      this.setState({
+        events: locationEvents.slice(0, eventCount),
+        numOfEvents: eventCount,
+        seletedLocation: location,
+      });
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <NumberOfEvents updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
       </div>
     );
   }
