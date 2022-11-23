cb4a72cfc7dfdbc637e19f31b4841acaad5f7929
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-11-23 11:46:27 +0100

    ErrorAlert added

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,25 +1,45 @@
 import React, { Component } from "react";
+import ErrorAlert from "./Alert";
 
 export class NumberOfEvents extends Component {
-  state = { numOfEvents: 16 };
+  state = {
+    numOfEvents: 16,
+    errorText: "",
+  };
   handelInputChanged = (event) => {
-    this.props.updateEvents(undefined, event.target.value);
     const value = event.target.value;
-    this.setState({ numOfEvents: value });
+    if (value < 1 || value > 16) {
+      this.setState({
+        numOfEvents: value,
+        errorText: "Enter number between 1 to 16",
+      });
+    } else {
+      this.setState({
+        numOfEvents: event.target.value,
+        errorText: "",
+      });
+    }
+    this.props.updateEvents(undefined, event.target.value);
   };
 
   render() {
     return (
-      <div className="numberOfEvents">
-        <label>
-          <p>Number of Events:</p>
-          <input
-            type="number"
-            className="number-input"
-            value={this.state.numOfEvents}
-            onChange={this.handelInputChanged}
-          />
-        </label>
+      <div>
+        <div className="numberOfEvents">
+          <label>
+            <p>Number of Events:</p>
+            <input
+              type="number"
+              className="number-input"
+              min="1"
+              value={this.state.numOfEvents}
+              onChange={this.handelInputChanged}
+            />
+          </label>
+        </div>
+        <div>
+          <ErrorAlert text={this.state.errorText} />
+        </div>
       </div>
     );
   }
